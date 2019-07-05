1f4500d45722043256c45a2a0977ebc1f59b5a18
Elad Cohen <eladc.android@gmail.com>
2019-07-05 14:04:40 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div style="border: 10px solid green; padding: 10px;">
+      <hello></hello>
+      <br />
+      <button (click)="onClick()">Trigger change detection</button>
+    </div>
+  `
 })
 export class AppComponent {
   title = 'changedetection';
+  onClick() {}
 }
